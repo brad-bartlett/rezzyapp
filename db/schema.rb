582f56ea9310bdcace22d5c61dd6e687cf86266c
iheartobi <iobidah9@gmail.com>
2019-07-11 14:04:20 -0400

    new working seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_10_134931) do
+ActiveRecord::Schema.define(version: 2019_07_10_221604) do
 
   create_table "friendships", force: :cascade do |t|
     t.integer "user_id"
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 2019_07_10_134931) do
   end
 
   create_table "reservations", force: :cascade do |t|
-    t.datetime "time"
+    t.string "time"
     t.string "name"
     t.string "restaurant"
     t.string "review"
@@ -53,9 +53,10 @@ ActiveRecord::Schema.define(version: 2019_07_10_134931) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
+    t.string "password_digest"
+    t.string "img_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
   end
 
 end
